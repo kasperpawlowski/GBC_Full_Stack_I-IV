673b7e3c081d2b28eca06bddbd41ad713d06a6cb
Kasper Pawlowski <kasperpawlowski@gmail.com>
2021-09-16 15:49:52 -0400

    exercise 3 reworked

--- Full_Stack_I/Lab 3/exercise_3.js
+++ Full_Stack_I/Lab 3/exercise_3.js
@@ -1,4 +1,4 @@
 const colors = ['red', 'green', 'blue'];
-const capitalizedColors = colors.map(element => element[0].toUpperCase() + element.slice(1));
+const capitalizedColors = colors.map(element => element[0].toUpperCase() + element.slice(1).toLowerCase());
 
 console.log(capitalizedColors);
