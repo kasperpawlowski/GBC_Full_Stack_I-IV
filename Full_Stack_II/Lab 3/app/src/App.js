0abc5943d2982ee40285c2aa9b9c5b3402c25ee7
Kasper Pawlowski <kasperpawlowski@gmail.com>
2021-10-12 21:50:06 -0400

    FS II Lab 3 updated

--- Full_Stack_II/Lab 3/app/src/App.js
+++ Full_Stack_II/Lab 3/app/src/App.js
@@ -2,7 +2,11 @@ import React from 'react';
 import TweetContainer from './components/TweetContainer';
 
 function App() {
-  return (<TweetContainer status="At home, binge watching Breaking Bad for second time!"/>);
+  return (<>
+            <TweetContainer status="At home, binge watching Breaking Bad for second time!"/>
+            <TweetContainer status="Will these be the best coins if remaining 2021!? What do u guys think, just comment!!"/>
+            <TweetContainer status="Rhis time $BTC is going to 0 for sure"/>
+          </>);
 }
 
 export default App;
